ff39f51979294d7f4c314082a9b72abee822b269
SahilLodhe <81958876+SahilLodhe@users.noreply.github.com>
2022-03-29 22:39:43 +0530

    Update find_the_duplicate_number.cpp

--- find_the_duplicate_number.cpp
+++ find_the_duplicate_number.cpp
@@ -1,6 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    unordered_map<string,int> myMap;
-    return 0;
-}
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> myMap;
+        for(int i = 0;i<nums.size();i++){
+            if(myMap.count(nums[i]) == 1){
+                return nums[i];
+            }
+            myMap[nums[i]] = true;
+        }
+        return 0;
+    }
+};
